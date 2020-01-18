aac8593c0a2aa622cddc9934f5745e767282967f
Daan Kauwenberg <daan.kauwenberg@gmail.com>
2020-01-18 17:18:53 +0100

    Add basic tests

--- test/test.js
+++ test/test.js
@@ -4,37 +4,40 @@ import { promises as fsPromises } from 'fs';
 
 import mjml from '../src';
 
-// const getCode = async (bundle, outputOptions, allFiles = false) => {
-//   const { output } = await bundle.generate(outputOptions || { format: 'cjs' });
-//   if (allFiles) {
-//     return output.map(({ code, fileName, source }) => {
-//       return { code, fileName, source };
-//     });
-//   }
-//   const [{ code }] = output;
-//   return code;
-// };
+const getCode = async (bundle, outputOptions) => {
+  const { output } = await bundle.generate(outputOptions || { format: 'esm' });
+  return output;
+};
 
-const output = { dir: 'output', format: 'esm' };
+const defaultConf = {
+  input: `./test/fixtures/template.mjml`,
+  output: {
+    format: 'esm',
+    dir: 'output'
+  },
+  plugins: [mjml()]
+}
 
 // Imported by JS
+test('exports when imported in Javascript', async (t) => {
+  t.plan(2);
+  const bundle = await rollup(Object.assign({}, defaultConf, {input: './test/fixtures/entry.js'}));
+  const output = await getCode(bundle ,defaultConf.output);
+  t.is(output.length, 2);
+  t.snapshot(output[1].source);
+});
 
 // Direct input
-test('direct input', async (t) => {
-  const conf = {
-    input: `./test/fixtures/template.mjml`,
-    output: {
-      format: 'esm',
-      dir: 'output'
-    },
-    plugins: [mjml()]
-  }
-  const bundle = await rollup(conf);
-  const { output } = await bundle.generate(conf.output);
-  console.log({output})
-  // const code = await getCode(bundle, output, true);
-  // console.log(code);
-  t.true(true);
+test('exports with .mjml input', async (t) => {
+  const bundle = await rollup(defaultConf);
+  const output = await getCode(bundle ,defaultConf.output);
+  t.snapshot(output[1].source);
 });
 
 // Can configure extension
+test('sets a non default file extension', async (t) => {
+  const testExtension = 'twig';
+  const bundle = await rollup(Object.assign({}, defaultConf, {plugins: [mjml({outputExt: testExtension})]}));
+  const output = await getCode(bundle ,defaultConf.output);
+  t.true(output[1].fileName.endsWith(testExtension));
+});
